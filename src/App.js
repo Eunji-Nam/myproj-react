195e52efc046896de314d8184cab01fb76b9cefc
Eunji Nam <godangel31@gmail.com>
2022-01-07 13:19:55 +0900

    TopNav 컴포넌트 스타일링

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import ReviewList from 'pages/reviews/ReviewList';
+import Profile from 'pages/accounts/Profile';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from 'pages/accounts/Login';
+import TopNav from 'compomemts/TopNav';
 
 function App() {
-  return <div>Hello React</div>;
+  return (
+    <div className="app">
+      <TopNav />
+      <Routes>
+        <Route path="/accounts/login/" element={<Login />} />
+        <Route path="/accounts/profile/" element={<Profile />} />
+        <Route path="/accounts/reviews/" element={<ReviewList />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
