8c7ffdbdffe04d5de1fcb76486953fd6d235e8a0
Eunji Nam <godangel31@gmail.com>
2022-01-07 14:31:20 +0900

    review list 조회 시에 loading/error 상탯값 처리 추가

--- src/pages/reviews/ReviewList.jsx
+++ src/pages/reviews/ReviewList.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import DebugStates from 'compomemts/DebugStates';
 
 function ReviewList() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [reviewList, setReviewList] = useState([]);
   // useEffect의 첫번째 인자는 함수, 두번째 인자는 배열
   useEffect(() => {
@@ -10,6 +12,9 @@ function ReviewList() {
   }, []);
 
   const refetch = () => {
+    setLoading(true);
+    setError(null);
+
     const url = 'http://127.0.0.1:8000/shop/api/reviews/';
     // Promise 객체
     Axios.get(url)
@@ -23,14 +28,28 @@ function ReviewList() {
         console.group('에러 응답');
         console.log(error);
         console.groupEnd();
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
 
   return (
     <div>
       <h2>Review List</h2>
+
+      {loading && <div>Loading ...</div>}
+      {error && <div>통신 중에 오류가 발생했습니다.</div>}
+
+      <button
+        onClick={() => refetch()}
+        className="bg-yellow-400 hover:bg-red-400"
+      >
+        새로고침
+      </button>
       <hr />
-      <DebugStates reviewList={reviewList} />
+      <DebugStates laoding={loading} error={error} reviewList={reviewList} />
     </div>
   );
 }
