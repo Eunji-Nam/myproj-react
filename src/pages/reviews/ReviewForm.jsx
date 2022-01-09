d8989ac47a3249bb3981b31a3324251d4577990a
Eunji Nam <godangel31@gmail.com>
2022-01-09 17:35:54 +0900

    ReviewForm로 새 리뷰 저장할 수 있도록 구현

--- src/pages/reviews/ReviewForm.jsx
+++ src/pages/reviews/ReviewForm.jsx
@@ -1,4 +1,33 @@
+import Axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 function ReviewForm() {
+  const [fieldValues, setfieldValues] = useState({
+    content: '',
+    score: 0,
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setfieldValues((prevFieldValues) => ({
+      ...prevFieldValues,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    const url = 'http://127.0.0.1:8000/shop/api/reviews/';
+    Axios.post(url, fieldValues)
+      .then(() => navigate('/reviews/'))
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => setfieldValues({}));
+  };
+
   return (
     <div className="rounded border-2 border-gray-300 p-3 my-3">
       <label className="block text-gray-700 text-sm font-bold mb-2 ">
@@ -6,6 +35,7 @@ function ReviewForm() {
       </label>
 
       <select
+        onChange={handleChange}
         name="score"
         className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
       >
@@ -21,11 +51,15 @@ function ReviewForm() {
       <input
         type="text"
         name="content"
+        onChange={handleChange}
         style={{ marginTop: 0, marginBottom: 12, height: 65 }}
         className="shadow appearance-none border border-gray-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
       />
 
-      <button className="shadow border bg-blue-100 hover:bg-blue-300 border-blue-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight">
+      <button
+        onClick={handleSubmit}
+        className="shadow border bg-blue-100 hover:bg-blue-300 border-blue-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight"
+      >
         저장하기
       </button>
     </div>
