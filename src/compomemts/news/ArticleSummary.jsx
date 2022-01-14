1a978b234bdc5e47ddf8af4d9675f7cfdf48782f
Eunji Nam <godangel31@gmail.com>
2022-01-14 10:59:16 +0900

    article list/Detail 컴포넌트에 photo 노출

--- src/compomemts/news/ArticleSummary.jsx
+++ src/compomemts/news/ArticleSummary.jsx
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom';
 function ArticleSummary({ article }) {
   return (
     <div>
-      요약: <Link to={`/news/${article.id}/`}>{article.title}</Link>
+      <img
+        src={article.photo}
+        alt={article.title}
+        className="w-5 h-5 mr-1 rounded inline"
+      />
+      <Link to={`/news/${article.id}/`}>{article.title}</Link>
     </div>
   );
 }
