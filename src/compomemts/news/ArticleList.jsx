f3251412e9ff65a643629a0d6cab944bef500031
Eunji Nam <godangel31@gmail.com>
2022-01-18 14:39:22 +0900

    author가 두번 나타나는 것을 수정

--- src/compomemts/news/ArticleList.jsx
+++ src/compomemts/news/ArticleList.jsx
@@ -39,7 +39,6 @@ function ArticleList() {
               key={article.id}
               className="w-full md:w-1/2 xl:w-1/3 px-4 transition-transform duration-300 hover:-translate-y-5"
             >
-              <p className="mb-3">by. {article.author.username}</p>
               <ArticleSummary article={article} />
             </div>
           ))}
